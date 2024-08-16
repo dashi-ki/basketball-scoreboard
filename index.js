dd4b9ca59fb409d57ac3f0ce50ff6a341bd33b65
Daria T <dtolkachova@protonmail.com>
2024-08-16 18:44:22 -0400

    Enhance functionality: Highlight the score display of the winning team in yellow.

--- index.js
+++ index.js
@@ -4,35 +4,60 @@ let guestScore = 0
 let homeScoreDisplay = document.getElementById("home-score")
 let guestScoreDisplay = document.getElementById("guest-score")
 
+
+function updateScores() {
+    homeScoreDisplay.textContent = homeScore
+    guestScoreDisplay.textContent = guestScore
+    
+    // Compare scores and update borders
+    if (homeScore > guestScore) {
+        homeScoreDisplay.style.border = "1px solid #FBBF24"
+        guestScoreDisplay.style.border = "1px solid #E5E7EB"
+    } else if (homeScore < guestScore) {
+        homeScoreDisplay.style.border = "1px solid #E5E7EB"
+        guestScoreDisplay.style.border = "1px solid #FBBF24"
+    } else {
+        homeScoreDisplay.style.border = "1px solid #E5E7EB"
+        guestScoreDisplay.style.border = "1px solid #E5E7EB"
+    }
+    updateScores()
+}
+
 function incrementHomeOne() {
     homeScore += 1
     homeScoreDisplay.textContent = homeScore
+    updateScores() 
 }
 
 function incrementHomeTwo() {
     homeScore += 2
     homeScoreDisplay.textContent = homeScore
+    updateScores() 
 }
 
 function incrementHomeThree() {
     homeScore += 3
     homeScoreDisplay.textContent = homeScore
+    updateScores() 
 }
 
 
 function incrementGuestOne() {
     guestScore += 1
     guestScoreDisplay.textContent = guestScore
+    updateScores() 
 }
 
 function incrementGuestTwo() {
     guestScore += 2
     guestScoreDisplay.textContent = guestScore
+    updateScores() 
 }
 
 function incrementGuestThree() {
     guestScore += 3
     guestScoreDisplay.textContent = guestScore
+    updateScores() 
 }
 
 function newGame() {
